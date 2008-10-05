0e9d48bec67e54921c4440726a9fb4c6f6f441c6
Geronimo Poppino <gresco@gmail.com>
2008-10-05 19:52:28 -0200

    WordsCommand: help + reformat

--- src/org/knix/amnotbot/WordsCommand.java
+++ src/org/knix/amnotbot/WordsCommand.java
@@ -3,14 +3,27 @@ package org.knix.amnotbot;
 import org.schwering.irc.lib.IRCUser;
 
 public class WordsCommand extends AmnotbotCommandImp {
-	
-	public WordsCommand()
-	{
-		super("^!(w(ords)?)\\s?(.*)", "w words");
-	}
 
-	public void execute(BotConnection con, String chan, IRCUser user, String msg) 
-	{
-		new WordsCommandThread(con, chan, user, this.getGroup(3), false);
-	}
+    public WordsCommand() {
+        super("^!(w(ords)?)\\s?(.*)", "w words");
+    }
+
+    public void execute(BotConnection con, String chan, IRCUser user,
+        String msg) {
+        new WordsCommandThread(con, chan, user, this.getGroup(3), false);
+    }
+
+    public String help() 
+    {
+        String msg;
+
+        msg = "!w or !words ";
+        msg += "Options: [ nick:n1,n2,n3 ";
+        msg += "word:w1,w2,w3 number:x1 ";
+        msg += "op:avg ] ";
+        msg += "\n Eg. !w number:10\n ";
+        msg += "\n !w nick:gresco word:cheeses";
+
+        return msg;
+    }
 }
