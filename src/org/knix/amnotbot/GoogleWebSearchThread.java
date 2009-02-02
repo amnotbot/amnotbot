c9bdec77b4f39651e35e6111a76b6a1aa54bcd38
Geronimo Poppino <gresco@gmail.com>
2009-02-02 19:57:23 -0200

    indentation google search

--- src/org/knix/amnotbot/GoogleWebSearchThread.java
+++ src/org/knix/amnotbot/GoogleWebSearchThread.java
@@ -18,88 +18,83 @@ import org.json.JSONObject;
  * @author gpoppino
  */
 public class GoogleWebSearchThread extends Thread {
-    
-	private BotConnection con;
-	private String query;
-	private String chan;
-	private String nick;
 
-    
-	public GoogleWebSearchThread(BotConnection con,
-			String chan,
-			String nick,
-			String query)
-	{
-		this.con = con;
-		this.chan = chan;
-		this.nick = nick;
-		this.query = query;
-	
-		start();
-	}
-    
-	public void run()
-	{
-		this.makeQuery();
-	}
-    
-	public void makeQuery()
-	{
-		URL url;
-		try {
-			String urlString = 
-				"http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" 
-				+ URLEncoder.encode(this.query, "UTF-8");
-			url = new URL(urlString);
-		} catch (MalformedURLException ex) {
-			BotLogger.getDebugLogger().debug(ex.getMessage());
-			return;
-		} catch (UnsupportedEncodingException ex) {
-			BotLogger.getDebugLogger().debug(ex.getMessage());
-			return;
-		}	
+    private BotConnection con;
+    private String query;
+    private String chan;
+    private String nick;
 
-		URLConnection connection;
-		try {
-			connection = url.openConnection();
-		} catch (IOException ex) {
-			BotLogger.getDebugLogger().debug(ex.getMessage());
-			return;
-		}
+    public GoogleWebSearchThread(BotConnection con,
+            String chan,
+            String nick,
+            String query) {
+        this.con = con;
+        this.chan = chan;
+        this.nick = nick;
+        this.query = query;
 
-		connection.addRequestProperty("Referer",
-			"http://knix.mine.nu/index.html");
+        start();
+    }
 
-		String line;
-		StringBuilder builder = new StringBuilder();
+    public void run() {
+        this.makeQuery();
+    }
 
-		try {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    public void makeQuery() {
+        URL url;
+        try {
+            String urlString =
+                    "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" + URLEncoder.encode(this.query, "UTF-8");
+            url = new URL(urlString);
+        } catch (MalformedURLException ex) {
+            BotLogger.getDebugLogger().debug(ex.getMessage());
+            return;
+        } catch (UnsupportedEncodingException ex) {
+            BotLogger.getDebugLogger().debug(ex.getMessage());
+            return;
+        }
 
-			while ((line = reader.readLine()) != null) {
-				builder.append(line);
-			}
-		} catch (IOException ex) {
-			BotLogger.getDebugLogger().debug(ex.getMessage());
-			return;
-		}
+        URLConnection connection;
+        try {
+            connection = url.openConnection();
+        } catch (IOException ex) {
+            BotLogger.getDebugLogger().debug(ex.getMessage());
+            return;
+        }
 
-		JSONObject json;
-		try {
-			json = new JSONObject(builder.toString());
-		} catch (JSONException ex) {
-			BotLogger.getDebugLogger().debug(ex.getMessage());
-			return;
-		}
-	
-		try {
-			this.con.doPrivmsg(this.chan, json.getJSONObject("responseData").getJSONArray("results").getJSONObject(0).optString("titleNoFormatting"));
-			this.con.doPrivmsg(this.chan, json.getJSONObject("responseData").getJSONArray("results").getJSONObject(0).optString("url"));
-			
-			String content = json.getJSONObject("responseData").getJSONArray("results").getJSONObject(0).optString("content");
-			this.con.doPrivmsg(this.chan, ParserUtils.trimAllTags(content, false));
-		} catch (JSONException ex) {
-			BotLogger.getDebugLogger().debug(ex.getMessage());
-		}
+        connection.addRequestProperty("Referer",
+                "http://knix.mine.nu/index.html");
+
+        String line;
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+        } catch (IOException ex) {
+            BotLogger.getDebugLogger().debug(ex.getMessage());
+            return;
+        }
+
+        JSONObject json;
+        try {
+            json = new JSONObject(builder.toString());
+        } catch (JSONException ex) {
+            BotLogger.getDebugLogger().debug(ex.getMessage());
+            return;
+        }
+
+        try {
+            this.con.doPrivmsg(this.chan, json.getJSONObject("responseData").getJSONArray("results").getJSONObject(0).optString("titleNoFormatting"));
+            this.con.doPrivmsg(this.chan, json.getJSONObject("responseData").getJSONArray("results").getJSONObject(0).optString("url"));
+
+            String content = json.getJSONObject("responseData").getJSONArray("results").getJSONObject(0).optString("content");
+            this.con.doPrivmsg(this.chan, ParserUtils.trimAllTags(content, false));
+        } catch (JSONException ex) {
+            BotLogger.getDebugLogger().debug(ex.getMessage());
+        }
     }
 }
