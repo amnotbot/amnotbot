f983f4e5f8a15942cfe85088fe493bc8f018e6d7
Geronimo Poppino <gresco@gmail.com>
2009-04-25 14:33:19 -0300

    botconnection code cleanup

--- src/org/knix/amnotbot/BotConnection.java
+++ src/org/knix/amnotbot/BotConnection.java
@@ -24,158 +24,140 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package org.knix.amnotbot;
-
 
 import org.schwering.irc.lib.IRCConnection;
 
-/**
- * IRCConnection wrapper implementing some useful methods.
- */
 public class BotConnection extends IRCConnection
 {
-	private BotLogger logger;
 
-	private boolean silent = false;
+    private BotLogger logger;
+    private boolean silent = false;
 
-	public BotConnection(String server,
-			int[] ports,
-			String passwd,
-			String nick,
-			String user,
-			String name)
-	{
-		super (server, ports, passwd, nick, user, name);
-	}
+    public BotConnection(String server,
+            int[] ports,
+            String passwd,
+            String nick,
+            String user,
+            String name)
+    {
+        super(server, ports, passwd, nick, user, name);
+    }
 
-	public BotConnection(String server,
-			int[] ports,
-			String passwd,
-			String nick)
-	{
-		super (server, ports, passwd, nick, nick, nick);
-	}
+    public BotConnection(String server,
+            int[] ports,
+            String passwd,
+            String nick)
+    {
+        super(server, ports, passwd, nick, nick, nick);
+    }
 
-	public BotConnection(String server,
-			int[] ports,
-			String nick)
-	{
-		super (server, ports, null, nick, nick, nick);
-	}
+    public BotConnection(String server,
+            int[] ports,
+            String nick)
+    {
+        super(server, ports, null, nick, nick, nick);
+    }
 
-	public BotConnection(String server, int port, String nick)
-	{
-		super (server, new int[] { port }, null, nick, nick, nick);
-	}
+    public BotConnection(String server, int port, String nick)
+    {
+        super(server, new int[]{port}, null, nick, nick, nick);
+    }
 
-	public BotConnection(String server, int port, String passwd, String nick)
-	{
-		super (server, new int[] { port }, passwd, nick, nick, nick);
-	}
+    public BotConnection(String server, int port, String passwd, String nick)
+    {
+        super(server, new int[]{port}, passwd, nick, nick, nick);
+    }
 
-	public BotConnection(String server, String passwd, String nick)
-	{
-		super(server, BotConstants.getBotConstants().getIrcPorts(), passwd, nick, nick, nick);
-	}
+    public BotConnection(String server, String passwd, String nick)
+    {
+        super(server, BotConstants.getBotConstants().getIrcPorts(),
+                passwd, nick, nick, nick);
+    }
 
-	public BotConnection(String server, String nick)
-	{
-		super (server, BotConstants.getBotConstants().getIrcPorts(), null, nick, nick, nick);
-	}
+    public BotConnection(String server, String nick)
+    {
+        super(server, BotConstants.getBotConstants().getIrcPorts(), null,
+                nick, nick, nick);
+    }
 
-	public BotConnection(String server)
-	{
-		super (server, 
-				BotConstants.getBotConstants().getIrcPorts(), 
-				null, 
-				BotConstants.getBotConstants().getNick(), 
-				BotConstants.getBotConstants().getNick(), 
-				BotConstants.getBotConstants().getNick()
-		);
-	}
+    public BotConnection(String server)
+    {
+        super(server,
+                BotConstants.getBotConstants().getIrcPorts(),
+                null,
+                BotConstants.getBotConstants().getNick(),
+                BotConstants.getBotConstants().getNick(),
+                BotConstants.getBotConstants().getNick());
+    }
 
-	public BotConnection(String server, int port)
-	{
-		super (server, 
-				new int[] { port }, 
-				null, 
-				BotConstants.getBotConstants().getNick(), 
-				BotConstants.getBotConstants().getNick(), 
-				BotConstants.getBotConstants().getNick()
-		);
-	}
+    public BotConnection(String server, int port)
+    {
+        super(server,
+                new int[]{port},
+                null,
+                BotConstants.getBotConstants().getNick(),
+                BotConstants.getBotConstants().getNick(),
+                BotConstants.getBotConstants().getNick());
+    }
 
-	public void setBotLogger(BotLogger logger)
-	{
-		this.logger = logger;
-	}
+    public void setBotLogger(BotLogger logger)
+    {
+        this.logger = logger;
+    }
 
-	/**
-	 * Not sure if I really want this to be available.
-	 * For now it's being used by other classes to facilitate getDefaultLogger()
-	 * for logging of stack traces
-	 * 
-	 * @return This Connection's BotLogger instance
-	 */
-	 public BotLogger getBotLogger()
-	{
-		 return logger;
-	}
+    public BotLogger getBotLogger()
+    {
+        return logger;
+    }
 
-	/**
-	 * Output messages we send to the console.
-	 */
-	 public void doPrivmsg(String target, String msg)
-	 {
-		 if (silent)
-			 return;
+    public void doPrivmsg(String target, String msg)
+    {
+        if (silent) return;
 
-		 super.doPrivmsg(target, msg);
+        super.doPrivmsg(target, msg);
 
-		 print(target, getNick() + "> " + msg);
-	 }
+        print(target, getNick() + "> " + msg);
+    }
 
-	 public void doNick(String nick)
-	 {
-		 print("doNick(" + nick + ") called.");
-		 super.doNick(nick);
-	 }
+    public void doNick(String nick)
+    {
+        print("doNick(" + nick + ") called.");
+        super.doNick(nick);
+    }
 
-	 public void setSilent(boolean silent)
-	 {
-		 this.silent = silent;
-	 }
+    public void setSilent(boolean silent)
+    {
+        this.silent = silent;
+    }
 
-	 public boolean isSilent()
-	 {
-		 return silent;
-	 }
+    public boolean isSilent()
+    {
+        return silent;
+    }
 
-	 /**
-	  * Method used to alter the bot's nick when we get a collision.
-	  * This could be greatly improved.
-	  */
-	 public void alternateNick()
-	 {
-		 String nick = getNick();
+    public void alternateNick()
+    {
+        String nick = getNick();
 
-		 if (nick.endsWith("_")) {
-			 doNick(nick.substring(0, nick.length()-1) + "-");
-		 } else {
-			 doNick(nick + "_");
-		 }
-	 }
+        if (nick.endsWith("_")) {
+            doNick(nick.substring(0, nick.length() - 1) + "-");
+        } else {
+            doNick(nick + "_");
+        }
+    }
 
-	 public void print(String msg)
-	 {
-		 if (logger != null)
-			 logger.log(msg);
-	 }
+    public void print(String msg)
+    {
+        if (logger != null) {
+            logger.log(msg);
+        }
+    }
 
-	 public void print(String target, String msg)
-	 {
-		 if (logger != null)
-			 logger.log(target.toLowerCase(), msg);
-	 }
+    public void print(String target, String msg)
+    {
+        if (logger != null) {
+            logger.log(target.toLowerCase(), msg);
+        }
+    }
 }
