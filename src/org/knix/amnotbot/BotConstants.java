211fccb6b27e6a0ce358f3d66b6ab5a004f591cb
Geronimo Poppino <gresco@gmail.com>
2009-04-25 15:12:16 -0300

    botconstants code cleanup

--- src/org/knix/amnotbot/BotConstants.java
+++ src/org/knix/amnotbot/BotConstants.java
@@ -24,77 +24,66 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package org.knix.amnotbot;
 
 import org.knix.amnotbot.config.BotConfiguration;
 
-/**
- * Constants used for the bot.
- */
 public class BotConstants
 {
-	/**
-	 * Default NICK of the bot.
-	 */
-	private final String NICK = "amcoolbot";
 
-	/** Prefix for server level messages */
-	private final String SERVER_PFX = "-!-";
+    private final String DEFAULT_NICK = "amcoolbot";
+    /** Prefix for server level messages */
+    private final String SERVER_PFX = "-!-";
+    /** Prefix for application level messages */
+    private final String APP_PFX = "***";
+    private final int IRC_PORTS[] = {6667, 6666, 6660, 6661, 6662,
+        6663, 6664, 6665, 6668, 6669};
+    private static BotConstants botConstants = null;
+    private String nick;
 
-	/** Prefix for application level messages */
-	private final String APP_PFX = "***";
+    protected BotConstants()
+    {
+        this.nick = null;
+    }
 
-	/** Ports to attempt to connect on */
-	private final int IRC_PORTS[] = { 6667, 6666, 6660, 6661, 6662,
-		6663, 6664, 6665, 6668, 6669 };
-	
-	private static BotConstants botConstants = null;
-	
-	private String nick;
-	
-	protected BotConstants()
-	{
-		this.nick = null;
-	}
-	
-	public static BotConstants getBotConstants()
-	{
-		if (botConstants == null)
-			botConstants = new BotConstants();
-		
-		return botConstants;				
-	}
-	
-	public String getNick() 
-	{
-		if (this.nick != null)
-			return this.nick;
-		
-		try {
-			this.nick = BotConfiguration.getConfig().getString("nick");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}			
-		
-		if (this.nick == null)
-			this.nick = this.NICK;
-		
-		return this.nick;
-	}
-	
-	public int [] getIrcPorts()
-	{
-		return this.IRC_PORTS;
-	}
-	
-	public String getServerPFX()
-	{
-		return this.SERVER_PFX;
-	}
-	
-	public String getAppPFX()
-	{
-		return this.APP_PFX;
-	}
+    public static BotConstants getBotConstants()
+    {
+        if (botConstants == null) {
+            botConstants = new BotConstants();
+        }
+
+        return botConstants;
+    }
+
+    public String getNick()
+    {
+        if (this.nick != null) return this.nick;
+
+        try {
+            this.nick = BotConfiguration.getConfig().getString("nick");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (this.nick == null) {
+            this.nick = this.DEFAULT_NICK;
+        }
+
+        return this.nick;
+    }
+
+    public int[] getIrcPorts()
+    {
+        return this.IRC_PORTS;
+    }
+
+    public String getServerPFX()
+    {
+        return this.SERVER_PFX;
+    }
+
+    public String getAppPFX()
+    {
+        return this.APP_PFX;
+    }
 }
