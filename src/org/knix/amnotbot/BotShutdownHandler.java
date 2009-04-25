99205944bca0a770cbc241ba693115d1b1d44d98
Geronimo Poppino <gresco@gmail.com>
2009-04-25 14:16:20 -0300

    botshutdownhandler code cleanup

--- src/org/knix/amnotbot/BotShutdownHandler.java
+++ src/org/knix/amnotbot/BotShutdownHandler.java
@@ -1,19 +1,18 @@
 package org.knix.amnotbot;
 
-public class BotShutdownHandler extends Thread {
-    
+public class BotShutdownHandler extends Thread
+{
+
     private IBot bot;
-    
-    public BotShutdownHandler(IBot bot) {
+
+    public BotShutdownHandler(IBot bot)
+    {
         this.bot = bot;
     }
-    
-    @Override
+
     public void run()
     {
         BotLogger.getDebugLogger().debug("Shutting down ...");
-        
         this.bot.shutdown();
     }
-
 }
