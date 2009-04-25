4bad42b06a0bb44122cea9eef4387acf7d3a1b8e
Geronimo Poppino <gresco@gmail.com>
2009-04-25 01:11:43 -0300

    BotConfiguration cleanup

--- src/org/knix/amnotbot/config/BotConfiguration.java
+++ src/org/knix/amnotbot/config/BotConfiguration.java
@@ -6,31 +6,31 @@ import org.apache.commons.configuration.PropertiesConfiguration;
 import org.apache.log4j.PropertyConfigurator;
 import org.knix.amnotbot.BotLogger;
 
-public class BotConfiguration {
+public class BotConfiguration
+{
 
-	private static BotConfiguration botConfig = null;
-	private static Configuration config = null;
-	
-	protected BotConfiguration() {
-		
-	}
-	
-	public static Configuration getConfig() 
-	{
-		if (botConfig == null) {
-			botConfig = new BotConfiguration();
-            
+    private static BotConfiguration botConfig = null;
+    private static Configuration config = null;
+
+    protected BotConfiguration()
+    {
+    }
+
+    public static Configuration getConfig()
+    {
+        if (botConfig == null) {
+            botConfig = new BotConfiguration();
+
             PropertyConfigurator.configure("log4j.properties");
-			
-			try {
-				config = new PropertiesConfiguration("amnotbot.config");
-			} catch (ConfigurationException e) {
-				e.printStackTrace();
-				BotLogger.getDebugLogger().debug("Problem loading amnotbot.config file");
-			}
-		}
-		
-		return config;
-	}
-	
+
+            try {
+                config = new PropertiesConfiguration("amnotbot.config");
+            } catch (ConfigurationException e) {
+                e.printStackTrace();
+                BotLogger.getDebugLogger().debug(e.getMessage());
+            }
+        }
+
+        return config;
+    }
 }
