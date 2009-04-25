0e8758ac1364f384da1845d297fa16f107748794
Geronimo Poppino <gresco@gmail.com>
2009-04-25 18:59:25 -0300

    qurlcommand code cleanup

--- src/org/knix/amnotbot/command/QurlRequestCommand.java
+++ src/org/knix/amnotbot/command/QurlRequestCommand.java
@@ -3,21 +3,16 @@ package org.knix.amnotbot.command;
 import org.knix.amnotbot.*;
 import org.schwering.irc.lib.IRCUser;
 
-/**
- * Created by IntelliJ IDEA.
- * User: gresco
- * Date: Oct 20, 2007
- * Time: 5:24:08 PM
- * To change this template use File | Settings | File Templates.
- */
-public class QurlRequestCommand extends BotCommandImp {
+public class QurlRequestCommand extends BotCommandImp
+{
 
-	public QurlRequestCommand() {
-		super("(https?://\\S{75,})", null);
-	}
+    public QurlRequestCommand()
+    {
+        super("(https?://\\S{75,})", null);
+    }
 
-	public void execute(BotConnection con, String chan, IRCUser user, String msg)
-	{
-		new QurlRequest(con, chan, user.getNick(), this.getGroup(1));
-	}
+    public void execute(BotConnection con, String chan, IRCUser user, String m)
+    {
+        new QurlRequest(con, chan, user.getNick(), this.getGroup(1));
+    }
 }
