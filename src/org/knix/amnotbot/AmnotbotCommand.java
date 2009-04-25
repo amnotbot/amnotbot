6b20eff3065299366b1dbcbbb436495667fc5f33
Geronimo Poppino <gresco@gmail.com>
2009-04-25 14:13:09 -0300

    amnotbotcommand code cleanup

--- src/org/knix/amnotbot/AmnotbotCommand.java
+++ src/org/knix/amnotbot/AmnotbotCommand.java
@@ -2,20 +2,16 @@ package org.knix.amnotbot;
 
 import org.schwering.irc.lib.IRCUser;
 
+public interface AmnotbotCommand
+{
 
-/**
- * Created by IntelliJ IDEA.
- * User: gresco
- * Date: Oct 20, 2007
- * Time: 2:43:47 PM
- */
-public interface AmnotbotCommand {
+    public boolean matches(String msg);
 
-	public boolean matches(String msg);
-	public void execute(BotConnection con, String chan, IRCUser user, String msg);
+    public void execute(BotConnection con, String chan, IRCUser user, String msg);
 
-	public String getKeywords();
-	public void setKeywords(String keywords);
+    public String getKeywords();
 
-	public String help(); 
+    public void setKeywords(String keywords);
+
+    public String help();
 }
