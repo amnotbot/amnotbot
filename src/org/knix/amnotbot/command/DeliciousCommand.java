b72057d3dbb1e469a76ad4f3b7db97c980410342
Geronimo Poppino <gresco@gmail.com>
2009-04-28 10:48:13 -0300

    delicious command code cleanup

--- src/org/knix/amnotbot/command/DeliciousCommand.java
+++ src/org/knix/amnotbot/command/DeliciousCommand.java
@@ -1,41 +1,46 @@
 package org.knix.amnotbot.command;
 
+import org.apache.commons.configuration.Configuration;
 import org.knix.amnotbot.*;
 import org.schwering.irc.lib.IRCUser;
 import org.knix.amnotbot.config.BotConfiguration;
 
 public class DeliciousCommand extends BotCommandImp
 {
-	private DeliciousBookmarks delicious;
-	private boolean showURL;
-	private int maxTagLength;
 
-	public DeliciousCommand(boolean showURL) 
-	{
-		super("^((http://([a-zA-Z]*.)?[a-zA-Z0-9]+(.[a-z]{2,4})+\\S*).*)", "delicious");
-		this.delicious = new DeliciousBookmarks();
-		this.showURL = showURL;
-		this.maxTagLength = 
-			BotConfiguration.getConfig().getInteger("delicious_max_tag_length", 30).intValue();
-	}
+    private boolean showURL;
+    private int maxTagLength;
+    private DeliciousBookmarks delicious;
 
-	public void execute(BotConnection con, String chan, IRCUser user, String msg)
-	{
-		new DeliciousThread(this.delicious, 
-					con, chan, user.getNick(), 
-					this.getGroup(2).split(" ")[0], 
-					msg, this.maxTagLength, this.showURL);
-	}
+    public DeliciousCommand(boolean showURL)
+    {
+        super("^((http://([a-zA-Z]*.)?[a-zA-Z0-9]+(.[a-z]{2,4})+\\S*).*)",
+                "delicious");
 
-	public String help()
-	{
-		String msg;
+        this.showURL = showURL;
+        this.delicious = new DeliciousBookmarks();
+        Configuration config = BotConfiguration.getConfig();
+        this.maxTagLength =
+                config.getInteger("delicious_max_tag_length", 30).intValue();
+    }
 
-		msg = "del.icio.us social bookmarking! ";
-		msg += "Options: [ title:\"your title\" ";
-		msg += "tags:abc,secondtag,thirdtag,def ";
-		msg += "comment:\"your comment\" ] ";
+    public void execute(BotConnection con, String chan, IRCUser user, String m)
+    {
+        new DeliciousThread(this.delicious,
+                con, chan, user.getNick(),
+                this.getGroup(2).split(" ")[0],
+                m, this.maxTagLength, this.showURL);
+    }
 
-		return msg;
-	}
+    public String help()
+    {
+        String msg;
+
+        msg = "del.icio.us social bookmarking! ";
+        msg += "Options: [ title:\"your title\" ";
+        msg += "tags:abc,secondtag,thirdtag,def ";
+        msg += "comment:\"your comment\" ] ";
+
+        return msg;
+    }
 }
