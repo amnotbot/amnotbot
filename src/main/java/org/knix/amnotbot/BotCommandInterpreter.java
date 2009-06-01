c281acb983091f2b21d81e0f7152751f413d2a35
Jim Mitchener <jcm@packetpan.org>
2009-06-01 01:00:26 -0800

    Minor refactoring. Remove trailing whitespace.

--- src/main/java/org/knix/amnotbot/BotCommandInterpreter.java
+++ src/main/java/org/knix/amnotbot/BotCommandInterpreter.java
@@ -10,10 +10,8 @@ import org.apache.commons.lang.StringUtils;
 import org.knix.amnotbot.config.BotConfiguration;
 import org.schwering.irc.lib.IRCUser;
 
-
 public class BotCommandInterpreter
 {
-
     private String cmdTrigger;
     private BotSpamDetector spamDetector;
     private LinkedList<BotCommand> linkListeners;
@@ -22,9 +20,10 @@ public class BotCommandInterpreter
     public BotCommandInterpreter(BotSpamDetector spamDetector)
     {    
         this.spamDetector = spamDetector;
-        this.cmdTrigger = BotConfiguration.getConfig().getString("command_trigger");     
-        this.cmdListeners = new 
-                HashMap<BotCommandEvent, LinkedList<BotCommand>>();
+        this.cmdTrigger =
+                BotConfiguration.getConfig().getString("command_trigger");
+        this.cmdListeners =
+                new HashMap<BotCommandEvent, LinkedList<BotCommand>>();
         this.linkListeners = new LinkedList<BotCommand>();
     }
 
@@ -32,13 +31,15 @@ public class BotCommandInterpreter
     {
         BotLogger.getDebugLogger().debug(command.getClass().getName());
         LinkedList<BotCommand> cmds;
+        
         if (this.cmdListeners.containsKey(e)) {
-            cmds = this.cmdListeners.get(e);            
+            cmds = this.cmdListeners.get(e);
         } else {
             cmds = new LinkedList<BotCommand>();
         }
+        
         cmds.add(command);
-        this.cmdListeners.put(e, cmds);     
+        this.cmdListeners.put(e, cmds);
     }
 
     public void addLinkListener(BotCommand command)
@@ -58,26 +59,25 @@ public class BotCommandInterpreter
     
     private boolean isLink(BotMessage msg)
     {
-        String url;
-        url = "^((http://([a-zA-Z]*.)?[a-zA-Z0-9]+(.[a-z]{2,4})+\\S*).*)";
-        Pattern p = Pattern.compile(url);
+        Pattern p = Pattern.compile(
+                "^((http://([a-zA-Z]*.)?[a-zA-Z0-9]+(.[a-z]{2,4})+\\S*).*)");
         Matcher m = p.matcher(msg.getText());
 
-        if (!m.find()) return false;
-        return true;
+        return m.find() ? true : false;
     }
 
     private String getTrigger(BotMessage msg)
     {
-        String trigger, text;
-                
-        text = msg.getText();
-        trigger = new String();
+        String text = msg.getText();
+        String trigger = new String();
+        
         for (int i = 1; i < text.length(); ++i) {
             char c = text.charAt(i);
-            if (!Character.isLetterOrDigit(c)) break;
-            trigger += c;
+            
+            if (Character.isLetterOrDigit(c))
+                trigger += c;
         }
+        
         return trigger;
     }
 
@@ -101,25 +101,30 @@ public class BotCommandInterpreter
     {
         String trigger = this.getTrigger(msg);
         Iterator<BotCommandEvent> it = this.cmdListeners.keySet().iterator();
+        
         while (it.hasNext()) {
-            BotCommandEvent event = it.next();            
-            if (event.test(trigger)) {               
+            BotCommandEvent event = it.next();
+            
+            if (event.test(trigger)) {
                 IRCUser user = msg.getUser();
                 String target = msg.getTarget();
-                if (this.spamDetector.checkForSpam(target, user)) {          
+                
+                if (this.spamDetector.checkForSpam(target, user)) {
                     BotLogger.getDebugLogger().debug("Spam Detected!");
                     return;
                 }
+                
                 this.removeTriggerString(msg);
                 this.execute(this.cmdListeners.get(event), msg);
             }
-        }     
+        }
     }
 
     private void execute(LinkedList<BotCommand> l, BotMessage msg)
     {
         Iterator<BotCommand> cmds;
         cmds = l.iterator();
+        
         while (cmds.hasNext()) {
             BotCommand command = cmds.next();
             command.execute(msg);
@@ -131,5 +136,5 @@ public class BotCommandInterpreter
         String text = msg.getText();
         text = text.substring(this.getTrigger(msg).length() + 1, text.length());
         msg.setText(text);
-    }    
+    }
 }
