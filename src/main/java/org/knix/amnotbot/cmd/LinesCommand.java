a51e5d54f493b53136b017d89194783fb28d7e4c
Geronimo Poppino <gresco@gmail.com>
2009-08-01 15:12:03 -0300

    Added support for Lines Command

--- src/main/java/org/knix/amnotbot/cmd/LinesCommand.java
+++ src/main/java/org/knix/amnotbot/cmd/LinesCommand.java
@@ -1,6 +1,12 @@
 package org.knix.amnotbot.cmd;
 
-import org.knix.amnotbot.*;
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import org.knix.amnotbot.BotCommand;
+import org.knix.amnotbot.BotMessage;
+import org.knix.amnotbot.cmd.utils.Utf8ResourceBundle;
+import org.knix.amnotbot.config.BotConfiguration;
 
 public class LinesCommand implements BotCommand
 {
@@ -19,6 +25,34 @@ public class LinesCommand implements BotCommand
     @Override
     public String help()
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Locale currentLocale;
+        ResourceBundle helpMessage;
+
+        currentLocale = new Locale(
+                BotConfiguration.getConfig().getString("language"),
+                BotConfiguration.getConfig().getString("country"));
+        helpMessage = Utf8ResourceBundle.getBundle("LinesCommandBundle",
+                currentLocale);
+
+        Object[] messageArguments = {
+            BotConfiguration.getConfig().getString("command_trigger"),
+            BotConfiguration.getCommandsConfig().getString("LinesCommand"),
+            helpMessage.getString("short_description"),
+            helpMessage.getString("options"),
+            helpMessage.getString("average"),
+            helpMessage.getString("nick"),
+            helpMessage.getString("nick_example"),
+            helpMessage.getString("date"),
+            helpMessage.getString("number"),
+            helpMessage.getString("date_description"),
+            helpMessage.getString("example")
+        };
+
+        MessageFormat formatter = new MessageFormat("");
+        formatter.setLocale(currentLocale);
+        formatter.applyPattern(helpMessage.getString("template"));
+
+        String output = formatter.format(messageArguments);
+        return output;
     }
 }
