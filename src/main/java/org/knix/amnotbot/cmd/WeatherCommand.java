fa3542c3f92449915c0df495ca2158e2f4cde7e7
Geronimo Poppino <gresco@gmail.com>
2009-08-01 18:15:06 -0300

    Added i18n support for WeatherCommand - help

--- src/main/java/org/knix/amnotbot/cmd/WeatherCommand.java
+++ src/main/java/org/knix/amnotbot/cmd/WeatherCommand.java
@@ -1,7 +1,12 @@
 package org.knix.amnotbot.cmd;
 
+import java.text.MessageFormat;
+import java.util.Locale;
+import java.util.ResourceBundle;
 import org.knix.amnotbot.BotCommand;
 import org.knix.amnotbot.BotMessage;
+import org.knix.amnotbot.cmd.utils.Utf8ResourceBundle;
+import org.knix.amnotbot.config.BotConfiguration;
 
 /**
  *
@@ -20,7 +25,31 @@ public class WeatherCommand implements BotCommand
     @Override
     public String help()
     {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Locale currentLocale;
+        ResourceBundle helpMessage;
+
+        currentLocale = new Locale(
+                BotConfiguration.getConfig().getString("language"),
+                BotConfiguration.getConfig().getString("country"));
+        helpMessage = Utf8ResourceBundle.getBundle("WeatherCommandBundle",
+                currentLocale);
+
+        Object[] messageArguments = {
+            BotConfiguration.getConfig().getString("command_trigger"),
+            BotConfiguration.getCommandsConfig().getString("WeatherCommand"),
+            helpMessage.getString("short_description"),
+            helpMessage.getString("options"),
+            helpMessage.getString("station_id"),
+            helpMessage.getString("example"),
+            helpMessage.getString("set_default_desc")
+        };
+
+        MessageFormat formatter = new MessageFormat("");
+        formatter.setLocale(currentLocale);
+        formatter.applyPattern(helpMessage.getString("template"));
+
+        String output = formatter.format(messageArguments);
+        return output;
     }
 
 }
