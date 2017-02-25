bb62a4a48d443671669225f0e38fa2a13ee663c1
Geronimo Poppino <gresco@gmail.com>
2017-02-25 19:28:45 -0300

    Implemented openweather command / Removed jweather implementation

---
+++ src/main/java/com/github/amnotbot/cmd/OpenWeatherImp.java
@@ -0,0 +1,103 @@
+package com.github.amnotbot.cmd;
+
+import com.github.amnotbot.BotMessage;
+import com.github.amnotbot.config.BotConfiguration;
+import net.aksingh.owmjapis.CurrentWeather;
+import net.aksingh.owmjapis.DailyForecast;
+import net.aksingh.owmjapis.OpenWeatherMap;
+import org.schwering.irc.lib.IRCConstants;
+
+import java.io.IOException;
+
+public class OpenWeatherImp
+{
+    private final BotMessage msg;
+    private final String city;
+
+    public OpenWeatherImp(BotMessage msg)
+    {
+        this.msg = msg;
+        this.city = msg.getText().trim();
+    }
+
+    public void run() throws IOException
+    {
+        String unit = BotConfiguration.getConfig().getString("openweather_unit");
+
+        OpenWeatherMap owm = null;
+        if (unit.compareTo("metric") == 0) {
+            owm = new OpenWeatherMap(OpenWeatherMap.Units.METRIC,
+                    BotConfiguration.getConfig().getString("openweather_key"));
+        } else {
+            owm = new OpenWeatherMap(OpenWeatherMap.Units.IMPERIAL,
+                    BotConfiguration.getConfig().getString("openweather_key"));
+        }
+
+        CurrentWeather cw = owm.currentWeatherByCityName(this.city);
+        if (cw.isValid()) {
+
+            String weatherString = IRCConstants.UNDERLINE_INDICATOR + "City: " + IRCConstants.UNDERLINE_INDICATOR +
+                    cw.getCityName();
+
+            if (cw.hasMainInstance()) {
+                if (cw.getMainInstance().hasTemperature()) {
+                    weatherString += IRCConstants.UNDERLINE_INDICATOR + " Current Temperature: " +
+                            IRCConstants.UNDERLINE_INDICATOR + cw.getMainInstance().getTemperature() + " ˚C";
+                }
+
+                if (cw.getMainInstance().hasMaxTemperature() && cw.getMainInstance().hasMinTemperature()) {
+                    weatherString += IRCConstants.UNDERLINE_INDICATOR + " Temperature Max/Min: " +
+                            IRCConstants.UNDERLINE_INDICATOR + cw.getMainInstance().getMaxTemperature() +
+                            "/" + cw.getMainInstance().getMinTemperature() + " ˚C";
+                }
+
+                if (cw.getMainInstance().hasHumidity()) {
+                    weatherString += IRCConstants.UNDERLINE_INDICATOR + " Humidity: " + IRCConstants.UNDERLINE_INDICATOR +
+                            cw.getMainInstance().getHumidity() + "%";
+                }
+
+                if (cw.getMainInstance().hasPressure()) {
+                    weatherString += IRCConstants.UNDERLINE_INDICATOR + " Pressure: " + IRCConstants.UNDERLINE_INDICATOR +
+                            cw.getMainInstance().getPressure();
+                }
+            }
+
+            if (cw.hasRainInstance()) {
+                if (cw.getRainInstance().hasRain()) {
+                    weatherString += IRCConstants.UNDERLINE_INDICATOR + " Rain: " + IRCConstants.UNDERLINE_INDICATOR +
+                            cw.getRainInstance().getRain() + "%";
+                }
+            }
+
+            if (cw.hasCloudsInstance()) {
+                if (cw.getCloudsInstance().hasPercentageOfClouds()) {
+                    weatherString += IRCConstants.UNDERLINE_INDICATOR +
+                            " Clouds: " + IRCConstants.UNDERLINE_INDICATOR +
+                            cw.getCloudsInstance().getPercentageOfClouds() + "%";
+                }
+            }
+
+            if (cw.hasSysInstance()) {
+                if (cw.getSysInstance().hasSunriseTime() && cw.getSysInstance().hasSunsetTime()) {
+                    weatherString += IRCConstants.UNDERLINE_INDICATOR + " Sunrise: " + IRCConstants.UNDERLINE_INDICATOR +
+                            cw.getSysInstance().getSunriseTime() + IRCConstants.UNDERLINE_INDICATOR + " Sunset: " +
+                            IRCConstants.UNDERLINE_INDICATOR + cw.getSysInstance().getSunsetTime();
+                }
+            }
+
+            if (cw.hasWindInstance()) {
+                if (cw.getWindInstance().hasWindSpeed() && cw.getWindInstance().hasWindDegree()) {
+                    String speed_unit = " miles/hour";
+                    if (unit.compareTo("metric") == 0) {
+                        speed_unit = " meter/sec";
+                    }
+                    weatherString += IRCConstants.UNDERLINE_INDICATOR + " Wind Speed: " + IRCConstants.UNDERLINE_INDICATOR +
+                            cw.getWindInstance().getWindSpeed() + speed_unit + IRCConstants.UNDERLINE_INDICATOR +
+                            " Wind Degree: " + IRCConstants.UNDERLINE_INDICATOR + cw.getWindInstance().getWindDegree();
+                }
+            }
+
+            this.msg.getConn().doPrivmsg(this.msg.getTarget(), weatherString);
+        }
+    }
+}
