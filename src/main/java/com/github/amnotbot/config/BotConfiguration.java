994bc644f3597747d7f90106e1d86ca05d684517
Geronimo Poppino <gpoppino@fastmail.net>
2020-05-11 13:00:08 -0300

    Do not exit the bot on first run
    We will be using environment variables

--- src/main/java/com/github/amnotbot/config/BotConfiguration.java
+++ src/main/java/com/github/amnotbot/config/BotConfiguration.java
@@ -103,8 +103,6 @@ public final class BotConfiguration
         System.out.println("\n\n");
         System.out.println(output);
         System.out.println("\n\n");
-        
-        System.exit(0);
     }
             
             
